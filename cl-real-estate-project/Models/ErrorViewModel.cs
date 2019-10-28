2e473e0c2b3bea8d8cbe45ab71ab4e04d26937ab
Jorge Saucedo <jsaucedo.developer@gmail.com>
2019-10-28 18:28:52 -0400

    Initial Commit

---
+++ cl-real-estate-project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cl_real_estate_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
